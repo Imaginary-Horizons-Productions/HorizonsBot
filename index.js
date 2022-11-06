1d0fb35555e1781cab2ad91e821a16b379e7a2d2
Nathaniel Tseng <nathanieltseng@gmail.com>
2022-11-06 11:25:33 -0800

    feat: decommission sharding

--- index.js
+++
@@ -1,8 +0,0 @@
-//TODONOW decommission
-const { ShardingManager } = require("discord.js");
-
-const manager = new ShardingManager("./source/bot.js", { token: require("./config/auth.json").token });
-
-manager.on("shardCreate", shard => console.log(`Launched shard ${shard.id}`));
-
-manager.spawn();
