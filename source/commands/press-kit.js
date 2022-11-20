bdaad1ae0a2d211649605eb710e9309f40779428
Nathaniel Tseng <nathanieltseng@gmail.com>
2022-11-20 15:19:17 -0800

    feat: ported press-kit command, added invite link

---
+++ source/commands/press-kit.js
@@ -0,0 +1,15 @@
+const { Interaction } = require('discord.js');
+const Command = require('../classes/Command.js');
+const embed = require("../../config/embeds/press-kit.json");
+
+const options = [];
+const subcommands = [];
+module.exports = new Command("press-kit", "Get info on Imaginary Horizons as a brand", false, options, subcommands);
+
+/** Private message user with IHC brand info
+ * @param {Interaction} interaction
+ */
+module.exports.execute = (interaction) => {
+	//TODO banner
+	interaction.reply({ embeds: [embed], ephemeral: true });
+}
