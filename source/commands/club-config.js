bf5182d71643dcf163b46d6bcd6a4aba6d04a7c5
Nathaniel Tseng <nathanieltseng@gmail.com>
2023-02-05 10:15:01 -0800

    #6: add permissions check to /club-config

--- source/commands/club-config.js
+++ source/commands/club-config.js
@@ -12,21 +12,25 @@ module.exports = new Command("club-config", "(club leader or moderator) Change t
  */
 module.exports.execute = (interaction) => {
 	const club = getClubDictionary()[interaction.channelId];
-	if (club) {
-		const { embeds } = clubInviteBuilder(club);
-		const components = [new ActionRowBuilder().addComponents(
-			new ButtonBuilder().setCustomId(`changeclubinfo${SAFE_DELIMITER}${club.id}`)
-				.setLabel("Set Name/Description")
-				.setStyle(ButtonStyle.Primary),
-			new ButtonBuilder().setCustomId(`changeclubseats${SAFE_DELIMITER}${club.id}`)
-				.setLabel("Set Members")
-				.setStyle(ButtonStyle.Primary),
-			new ButtonBuilder().setCustomId(`changeclubmeeting${SAFE_DELIMITER}${club.id}`)
-				.setLabel("Set Meeting Time")
-				.setStyle(ButtonStyle.Primary)
-		)];
-		interaction.reply({ embeds, components, ephemeral: true });
-	} else {
-		interaction.reply({ content: "Please configure club settings from the club's text channel.", ephemeral: true });
+	if (!club) {
+		return interaction.reply({ content: "Please configure club settings from the club's text channel.", ephemeral: true });
 	}
+
+	if (!isModerator(interaction.user.id) && interaction.user.id !== club.hostId) {
+		return interaction.reply({ content: "Promoting a club leader is restricted to the current club leader and Moderators.", ephemeral: true });
+	}
+
+	const { embeds } = clubInviteBuilder(club);
+	const components = [new ActionRowBuilder().addComponents(
+		new ButtonBuilder().setCustomId(`changeclubinfo${SAFE_DELIMITER}${club.id}`)
+			.setLabel("Set Name/Description")
+			.setStyle(ButtonStyle.Primary),
+		new ButtonBuilder().setCustomId(`changeclubseats${SAFE_DELIMITER}${club.id}`)
+			.setLabel("Set Members")
+			.setStyle(ButtonStyle.Primary),
+		new ButtonBuilder().setCustomId(`changeclubmeeting${SAFE_DELIMITER}${club.id}`)
+			.setLabel("Set Meeting Time")
+			.setStyle(ButtonStyle.Primary)
+	)];
+	interaction.reply({ embeds, components, ephemeral: true });
 }
