3a2d8e6071ce5ef4c4e6246b5289d0b01bae38cc
Nathaniel Tseng <nathanieltseng@gmail.com>
2023-05-19 09:21:36 -0700

    #72: Port script improvements

---
+++ source/scripts/clearCommands.js
@@ -0,0 +1,28 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { token, botId, guildId } = require('../../config/auth.json');
+
+const rest = new REST({ version: 9 }).setToken(token);
+
+(async () => {
+	try {
+		console.log('Started clearing slash commands.');
+
+		if (botId) {
+			await rest.put(Routes.applicationCommands(botId), { body: [] });
+
+			if (guildId) {
+				await rest.put(
+					Routes.applicationGuildCommands(botId, guildId),
+					{ body: [] },
+				);
+			}
+		} else {
+			throw new Error("Could not clear commands due to botId missing from config")
+		}
+
+		console.log('Successfully cleared slash commands.');
+	} catch (error) {
+		console.error(error);
+	}
+})();
