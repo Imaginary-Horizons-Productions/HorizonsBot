e8db9f42b4972a2a31a87c1d50b3dee9baa611b2
Nathaniel Tseng <nathanieltseng@gmail.com>
2023-06-09 11:02:24 -0700

    #73: port /petition-veto

--- source/commands/_commandDictionary.js
+++ source/commands/_commandDictionary.js
@@ -6,11 +6,11 @@ exports.commandSets = [
 	new CommandSet("General Commands", "These commands are general use utilities for the server.", false,
 		["at-channel.js", "timestamp.js", "roll.js", "petition.js"]),
 	new CommandSet("Informantional Commands", "Use these commands to learn more about this server or HorizonsBot.", false,
-		["info.js", "commands.js", "list.js", "version.js"]),
+		["info.js", "commands.js", "list.js", "version.js", "petition-check.js"]),
 	new CommandSet("Club Commands", "Clubs are private text and voice channels that include organization utilities like automatic reminders.", false,
 		["club-invite.js", "club-kick.js", "club-leave.js", "club-send-reminder.js", "club-config.js", "club-promote-host.js", "club-add.js", "club-sunset.js"]),
 	new CommandSet("Moderation Commands", "Commands for moderators. Required permissions are listed in (parenthesis) at the beginning of the description.", true,
-		["at-permission.js", "petition-check.js", "topic-add.js", "manage-mods.js", "post-reference.js"])
+		["at-permission.js", "petition-veto.js", "topic-add.js", "manage-mods.js", "post-reference.js"])
 ];
 
 exports.commandFiles = exports.commandSets.reduce((allFiles, set) => allFiles.concat(set.fileNames), []);
