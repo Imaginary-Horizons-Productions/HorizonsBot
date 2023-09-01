bed0af2c08885d674ef59fcb1f93f4f0a4be1ca0
Nathaniel Tseng <nathanieltseng@gmail.com>
2023-09-01 13:05:06 -0700

    #101: resolve permission misconfiguration for club leader (#105)

--- source/commands/club-kick.js
+++ source/commands/club-kick.js
@@ -8,7 +8,7 @@ const options = [
 	{ type: "Boolean", name: "ban", description: "Prevent the user from rejoining?", required: false, choices: [] }
 ];
 const subcomands = [];
-module.exports = new Command(id, "Remove a user from a club", false, PermissionFlagsBits.ManageChannels, 3000, options, subcomands);
+module.exports = new Command(id, "Remove a user from a club", false, PermissionFlagsBits.ManageMessages, 3000, options, subcomands);
 
 /** Remove visibility of receiving channel from mentioned user
  * @param {import('discord.js').Interaction} interaction
