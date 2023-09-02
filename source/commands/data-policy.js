18ed926d770f269893544060c88b38adb087ee44
Nathaniel Tseng <nathanieltseng@gmail.com>
2023-09-01 17:42:28 -0700

    #100: Switch `/data-policy` to "link to wiki" style

---
+++ source/commands/data-policy.js
@@ -0,0 +1,12 @@
+const { PermissionFlagsBits } = require('discord.js');
+const Command = require('../classes/Command');
+
+const customId = "data-policy";
+const options = [];
+const subcommands = [];
+module.exports = new Command(customId, "Get a link to the HorizonsBot's Data Policy page", PermissionFlagsBits.ViewChannel, false, true, 3000, options, subcommands,
+	/** Send the user a link to the Data Policy wiki page */
+	(interaction) => {
+		interaction.reply({ content: "Here's a link to the HorizonsBot Data Policy page (automatically updated): https://github.com/Imaginary-Horizons-Productions/HorizonsBot/wiki/Data-Policy", ephemeral: true });
+	}
+);
