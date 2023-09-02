ff78d2e6d5d9926cda25db6a5326fd3332151a18
Nathaniel Tseng <nathanieltseng@gmail.com>
2023-09-01 17:47:46 -0700

    update /data-policy to HorizonsBot command structure

--- source/commands/data-policy.js
+++ source/commands/data-policy.js
@@ -4,9 +4,10 @@ const Command = require('../classes/Command');
 const customId = "data-policy";
 const options = [];
 const subcommands = [];
-module.exports = new Command(customId, "Get a link to the HorizonsBot's Data Policy page", PermissionFlagsBits.ViewChannel, false, true, 3000, options, subcommands,
+module.exports = new Command(customId, "Get a link to the HorizonsBot's Data Policy page", true, PermissionFlagsBits.ViewChannel, 3000, options, subcommands,
 	/** Send the user a link to the Data Policy wiki page */
-	(interaction) => {
-		interaction.reply({ content: "Here's a link to the HorizonsBot Data Policy page (automatically updated): https://github.com/Imaginary-Horizons-Productions/HorizonsBot/wiki/Data-Policy", ephemeral: true });
-	}
 );
+
+module.exports.execute = (interaction) => {
+	interaction.reply({ content: "Here's a link to the HorizonsBot Data Policy page (automatically updated): https://github.com/Imaginary-Horizons-Productions/HorizonsBot/wiki/Data-Policy", ephemeral: true });
+}
