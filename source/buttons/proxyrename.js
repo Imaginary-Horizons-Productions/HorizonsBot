906f3f4aee04bc87d5715008704bbf0209869d85
Nathaniel Tseng <nathanieltseng@gmail.com>
2025-10-20 11:16:52 -0700

    update proxy rename modal to use LabelComponents - like with previous updates, dropped placeholder length limit message as Discord's UI communicates that dynamically - replace markdown with formatter

--- source/buttons/proxyrename.js
+++ source/buttons/proxyrename.js
@@ -1,5 +1,5 @@
 const { ButtonWrapper } = require('../classes');
-const { TextInputStyle, ModalBuilder, TextInputBuilder, ActionRowBuilder } = require('discord.js');
+const { TextInputStyle, ModalBuilder, TextInputBuilder, LabelBuilder, bold } = require('discord.js');
 const { timeConversion } = require('../util/mathUtil');
 const { SKIP_INTERACTION_HANDLING, SAFE_DELIMITER } = require('../constants');
 
@@ -9,21 +9,20 @@ module.exports = new ButtonWrapper(mainId, 3000,
 		const modalCustomId = `${SKIP_INTERACTION_HANDLING}${SAFE_DELIMITER}${interaction.id}`;
 		interaction.showModal(new ModalBuilder().setCustomId(modalCustomId)
 			.setTitle("Rename Thread")
-			.setComponents(
-				new ActionRowBuilder().addComponents(
-					new TextInputBuilder().setCustomId("title")
-						.setLabel("New Name")
-						.setValue(interaction.channel.name)
-						.setStyle(TextInputStyle.Short)
-						.setMinLength(1)
-						.setMaxLength(100)
-						.setPlaceholder("1-100 characters")
-				)
+			.addLabelComponents(
+				new LabelBuilder().setLabel("New Name")
+					.setTextInputComponent(
+						new TextInputBuilder().setCustomId("title")
+							.setValue(interaction.channel.name)
+							.setStyle(TextInputStyle.Short)
+							.setMinLength(1)
+							.setMaxLength(100)
+					)
 			));
 		interaction.awaitModalSubmit({ filter: submission => submission.customId === modalCustomId, time: timeConversion(5, "m", "ms") }).then(modalSubmission => {
 			const newName = modalSubmission.fields.fields.get("title").value;
 			interaction.channel.setName(newName);
-			modalSubmission.reply(`${interaction.member} renamed this thread to **${newName}**.`);
+			modalSubmission.reply(`${interaction.member} renamed this thread to ${bold(newName)}.`);
 		})
 	}
 );
